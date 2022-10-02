52203ccd97378922f850fccaddd5304642c24957
ekfman <ekfirstman@gmail.com>
2022-10-02 15:38:27 -0700

    day 3 finished

--- api/tags.js
+++ api/tags.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const tagsRouter = express.Router();
+const { getAllTags,getPostsByTagName } = require('../db');
 
 tagsRouter.use( (req, res, next) => {
     console.log("A request is being made to /tags");
@@ -7,7 +8,6 @@ tagsRouter.use( (req, res, next) => {
     next();
   });
 
-const { getAllTags } = require('../db');
 
 tagsRouter.get('/', async (req, res) => {
     const tags = await getAllTags();
@@ -15,5 +15,26 @@ tagsRouter.get('/', async (req, res) => {
         tags
     });
 });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params;
+
+    try {
+        const postByTagName = await getPostsByTagName(tagName)
+        if (postByTagName){
+        res.send( {posts: postByTagName })
+    } else{
+        next({
+            name: 'Unknown Tag',
+            message: 'Cannot find that specific tag'
+          }) 
+    }
+
+
+    } catch ({ name, message }) {
+      next({ name, message } )
+    }
+  });
+
 
 module.exports = tagsRouter
